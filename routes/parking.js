2c5c848343f5fd9e8975771f3ff867076ba1d934
Charlie Kelly <charliekelly31415@gmail.com>
2025-10-22 18:51:57 -0400

    update controllers

--- routes/parking.js
+++ routes/parking.js
@@ -1,5 +1,4 @@
-const express = require("express");
-const router = express.Router();
+const router = require("express").Router();
 const upload = require("../middleware/multer");
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
