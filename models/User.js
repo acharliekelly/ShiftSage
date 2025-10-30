b82354406047baa361b59651a906eac3464b3d44
Charlie Kelly <charliekelly31415@gmail.com>
2025-10-30 12:58:15 -0400

    use default User.save

--- models/User.js
+++ models/User.js
@@ -14,26 +14,9 @@ UserSchema.methods.validatePassword = function(pw) {
   return bcrypt.compare(pw, this.passwordHash);
 }
 
-// Hash the password before saving the user
-UserSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    return next();
-  }
-  // save
-  try {
-    // ?
-    if (this.passwordHash && this.passwordHash.length) {
-      this.setPassword(this.passwordHash);
-      return next();
-    } else {
-      // error
-      console.log("No password!!");
-    }
-  } catch (err) {
-    console.err(err);
-    next(err);
-  }
-});
+UserSchema.methods.hasPassword() = async function() {
+  return this.passwordHash && this.passwordHash.length;
+}
 
 
 module.exports = mongoose.model("User", UserSchema);
